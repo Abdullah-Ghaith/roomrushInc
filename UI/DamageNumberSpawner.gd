826ac20b8f8fe7a491a9f94d1f52367530efcb3d
abdullah-ghaith <abdullah.ghaith3@gmail.com>
2026-04-01 11:33:08 -0400

    add damage number spawner

---
+++ UI/DamageNumberSpawner.gd
@@ -0,0 +1,34 @@
+class_name DamageNumberSpawner extends Node2D
+
+const TOP_Z_LEVEL : int = 1000
+
+@export var label_settings: LabelSettings
+@export var critical_hit_color: Color = Color(0.0, 0.5, 0.5, 1.0)
+
+@onready var health_component: HealthComponent = %HealthComponent
+
+func _ready() -> void:
+	health_component.damaged.connect(func(_amount):
+		spawn_label(_amount, false))
+
+func spawn_label(number: float, critical_hit: bool = false) -> void:
+	var new_label: Label = Label.new()
+	
+	new_label.text = str(number)
+	new_label.label_settings = label_settings.duplicate()
+	new_label.z_index = TOP_Z_LEVEL
+	new_label.pivot_offset = Vector2(0.5, 1.0)
+
+	if critical_hit:
+		new_label.label_settings.font_color = critical_hit_color
+	
+	call_deferred("add_child", new_label)
+	await new_label.resized
+	new_label.position -= Vector2(new_label.size.x / 2.0, new_label.size.y)
+
+	var target_rise_pos: Vector2 = new_label.position + Vector2(randf_range(-5.0,5.0), randf_range(-22.0, -16.0))
+	var tween_length: float = 0.92
+	var label_tween: Tween = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	label_tween.tween_property(new_label, "position", target_rise_pos, tween_length)
+	label_tween.parallel().tween_property(new_label, "scale", Vector2.ONE * 1.35, tween_length)
+	label_tween.parallel().tween_property(new_label, "modulate:a", 0.0, tween_length).connect("finished", new_label.queue_free)
