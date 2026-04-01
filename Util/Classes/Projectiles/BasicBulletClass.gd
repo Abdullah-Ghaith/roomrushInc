961d46f5e133d5e076305df514c09190a518a923
abdullah-ghaith <abdullah.ghaith3@gmail.com>
2026-04-01 15:22:56 -0400

    add hit splash

--- Util/Classes/Projectiles/BasicBulletClass.gd
+++ Util/Classes/Projectiles/BasicBulletClass.gd
@@ -1,6 +1,7 @@
 class_name basicBullet extends Area2D
 
 @export var bullet_data : projectileData
+@export var HIT_FX : Array[PackedScene]
 
 func _ready() -> void:
 	self.top_level = true
@@ -16,9 +17,21 @@ func _process(delta: float) -> void:
 
 func _on_hitbox_entered(area: Area2D) -> void:
 	if area is HitboxComponent:
+		## Compose attack
 		var attack = Attack.new()
 		attack.attack_damage   = bullet_data.damage
 		attack.attack_position = self.global_position
 		attack.knockback_force = bullet_data.knockback
 		area.take_damage(attack)
+		
+		## Spawn in VFX
+		for fx in HIT_FX:
+			var fx_node = fx.instantiate()
+			# add rand for juice (TODO? Could make these rand values a part of the data class)
+			fx_node.global_position = global_position + Vector2(randf_range(-8, 8), randf_range(-8, 8)) 
+			fx_node.scale = Vector2.ONE * randf_range(0.8, 1.2)
+			fx_node.rotation = randf_range(-0.3, 0.3)
+			# add to root so it doesn't get freed with the bullet
+			get_tree().root.add_child(fx_node)  
+		
 		queue_free()
